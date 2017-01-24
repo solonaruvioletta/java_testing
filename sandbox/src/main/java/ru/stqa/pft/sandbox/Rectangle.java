a62e94f4a3d11e944110d27469807091e7d474c2
solonaruvioletta <violetta.solonaru@gmail.com>
2017-01-25 00:03:25 +0300

    Добавлены новые классы для описания квадратов и прямоугольников

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Rectangle.java
@@ -0,0 +1,20 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by 1 on 24.01.2017.
+ */
+public class Rectangle {
+  public double a;
+  public double b;
+
+  public Rectangle(double a, double b) {
+    this.a = a;
+    this.b = b;
+  }
+
+  public double area() {
+    return this.a * this.b;
+  }
+
+
+}
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by 1 on 24.01.2017.
+ */
+public class Square {
+
+  public double l;
+
+  public Square(double l) {
+    this.l = l;
+  }
+
+  public double area() {
+    return this.l * this.l;
+  }
+
+
+}
