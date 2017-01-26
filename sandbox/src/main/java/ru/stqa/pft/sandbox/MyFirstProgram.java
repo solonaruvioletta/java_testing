9a3fa1eabf7708515c0b66daa296aa10a753b8bc
Violetta <violetta.solonaru@gmail.com>
2017-01-26 16:04:48 +0300

    Без оптимизации. Расстояние между двумя точками

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -12,12 +12,29 @@ public class MyFirstProgram {
 
     Rectangle r = new Rectangle(4, 6);
     System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
+    Point p1 = new Point();
+    p1.x = 4;
+    p1.y = 3;
+    
+    Point p2 = new Point();
+    p2.x = 8;
+    p2.y = 6;
+    System.out.println("Расстояние между точками p1(" + p1.x + "; " + p1.y + ") и p2(" + p2.x + "; " + p2.y + ") равно " + distance(p1, p2));
   }
 
   public static void hello(String somebody) {
     System.out.println("Hello, " + somebody + "!");
   }
 
+  public static double distance(Point p1, Point p2) {
+
+    return Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
+
+  }
+
 
 }
+
+
 
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,11 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by user on 26.01.2017.
+ */
+public class Point {
+
+  public double x;
+  public double y;
+
+}
