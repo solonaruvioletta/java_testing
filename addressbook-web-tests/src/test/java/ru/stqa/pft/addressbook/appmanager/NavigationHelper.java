4c65846c04b9448e1c770d7e2b66e325d3a87223
Violetta <violetta.solonaru@gmail.com>
2017-02-09 19:00:57 +0300

    Оптимизированы переходы между страницами ч.1

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -13,6 +13,11 @@ public class NavigationHelper extends HelperBase {
   }
 
   public void goToGroupPage() {
+    if (isElementPresent(By.tagName("h1"))
+            && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+            && isElementPresent(By.name("new"))) {
+      return;
+    }
     click(By.linkText("groups"));
   }
 
@@ -20,5 +25,10 @@ public class NavigationHelper extends HelperBase {
     click(By.linkText("add new"));
   }
 
-  public void goToHomePage() { click(By.linkText("home")); }
+  public void goToHomePage() {
+    if (isElementPresent(By.id("maintable"))) {
+      return;
+    }
+    click(By.linkText("home"));
+  }
 }
