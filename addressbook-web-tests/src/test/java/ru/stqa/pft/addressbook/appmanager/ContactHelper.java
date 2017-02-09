bc54adef9b3cec94bffb5bb59132fbf9ba367de9
solonaruvioletta <violetta.solonaru@gmail.com>
2017-02-09 23:49:30 +0300

    Черновой тест "Контакт" ч.2

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -64,7 +64,6 @@ public class ContactHelper extends HelperBase {
   public void createContact(ContactData contact, boolean b) {
     fillContactForm(contact, b);
     enterContactInformation();
-    goToMainPage();
   }
 
   public boolean isThereAContact() {
