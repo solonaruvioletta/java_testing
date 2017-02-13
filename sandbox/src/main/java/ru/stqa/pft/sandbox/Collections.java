0dec628329ea3dd6de403a3f5b88ca2a6cd4c201
Violetta <violetta.solonaru@gmail.com>
2017-02-13 19:00:01 +0300

    Коллекции

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
@@ -0,0 +1,21 @@
+package ru.stqa.pft.sandbox;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by user on 13.02.2017.
+ */
+public class Collections {
+
+  public static void main(String[] args) {
+    String[] langs = {"Java", "C#", "Python", "PHP"};
+
+    List<String> languages = Arrays.asList("Java", "C#", "Python", "PHP");
+
+    for (String l : languages) {
+      System.out.println("Я хочу выучить " + languages.get(i));
+    }
+  }
+}
