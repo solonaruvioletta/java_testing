2e55d3fedd860765adad630ed8a42a531dba5041
Violetta <violetta.solonaru@gmail.com>
2017-02-14 19:21:56 +0300

    Выбор по порядковому номеру (группы и контакты)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -45,8 +45,8 @@ public class ContactHelper extends HelperBase {
     click(By.name("theform"));
   }
 
-  public void selectContact() {
-    click(By.name("selected[]"));
+  public void selectContact(int index) {
+    wd.findElements(By.name("selected[]")).get(index).click();
   }
 
   public void deleteSelectedContacts() {
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -27,7 +27,7 @@ public class ContactDeletionTests extends TestBase {
     }
     app.getNavigationHelper().goToHomePage();
     int before = app.getContactHelper().getContactCount();
-    app.getContactHelper().selectContact();
+    app.getContactHelper().selectContact(before - 1);
     app.getContactHelper().deleteSelectedContacts();
     app.getContactHelper().acceptContactDeletion();
     app.getNavigationHelper().goToHomePage();
