5e08e318052b8a6dd8de27794a7fc8ff5df2b74c
Violetta <violetta.solonaru@gmail.com>
2017-02-21 15:47:34 +0300

    Добавлены предусловия (контакт: адрес, телефоны, имейлы)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactAddressTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactAddressTests.java
@@ -1,7 +1,9 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -10,6 +12,19 @@ import static org.hamcrest.MatcherAssert.assertThat;
  * Created by user on 20.02.2017.
  */
 public class ContactAddressTests extends TestBase {
+
+  @BeforeMethod
+  public void ensurePreconditions() {
+    app.goTo().homePage();
+    if (app.contact().all().size() == 0) {
+      app.goTo().groupPage();
+      if (app.group().all().size() == 0) {
+        app.group().create(new GroupData().withName("test1"));
+      }
+      app.goTo().addNewPage();
+      app.contact().create(new ContactData().withFirstname("Violetta").withMiddlename("Igorevna").withLastname("Solonaru").withNickname(null).withTitle(null).withCompany(null).withAddress(null).withHomephone(null).withMobilephone(null).withWorkphone(null).withFax(null).withEmail(null).withEmail2(null).withBirthyear(null).withGroup("test1"), true);
+    }
+  }
 
   @Test
   public void testContactAddress() {
