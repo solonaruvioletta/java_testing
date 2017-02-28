b4f5d0c4870680bca3ec3bef8067dbee9c7169b5
Violetta <violetta.solonaru@gmail.com>
2017-02-28 20:39:34 +0300

    модуль 10. 11:12

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -10,7 +10,8 @@ import org.openqa.selenium.remote.BrowserType;
  */
 public class TestBase {
 
-  protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+  protected static final ApplicationManager app
+          = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
   @BeforeSuite
   public void setUp() throws Exception {
