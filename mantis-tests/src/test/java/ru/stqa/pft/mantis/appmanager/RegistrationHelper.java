f44436bcdcbf3e42796f28f521a51aa54d2f1931
Violetta <violetta.solonaru@gmail.com>
2017-03-16 19:01:34 +0300

    урок 8 модуль 4

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
@@ -0,0 +1,23 @@
+package ru.stqa.pft.mantis.appmanager;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by user on 16.03.2017.
+ */
+public class RegistrationHelper {
+  private final ApplicationManager app;
+  private WebDriver wd;
+
+  public RegistrationHelper(ApplicationManager app) {
+    this.app = app;
+    wd = app.getDriver();
+
+  }
+
+  public void start(String username, String email) {
+    wd.get(app.getProperty("web.baseUrl") + "/signup_page.php");
+
+
+  }
+}
