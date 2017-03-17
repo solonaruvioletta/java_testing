490af5fe831c4229de435875054cf43ba1d4e01c
Violetta <violetta.solonaru@gmail.com>
2017-03-17 19:58:51 +0300

    пауза 6:09, модуль 6

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/MailHelper.java
@@ -0,0 +1,44 @@
+package ru.stqa.pft.mantis.appmanager;
+
+import com.sun.xml.internal.messaging.saaj.packaging.mime.MessagingException;
+import org.subethamail.wiser.Wiser;
+import org.subethamail.wiser.WiserMessage;
+
+import javax.mail.internet.MimeMessage;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Created by user on 17.03.2017.
+ */
+public class MailHelper {
+  private ApplicationManager app;
+  private final Wiser wiser;
+
+  public MailHelper(ApplicationManager app) {
+    this.app = app;
+    wiser = new Wiser;
+  }
+
+  public List<MailMessage> waitForMail(int count, long timeout) throws MessagingException, IOException {
+    long start = System.currentTimeMillis();
+    while (System.currentTimeMillis() < start + timeout) {
+      if (wiser.getMessages().size() >= count) {
+        return wiser.getMessages().stream().map((m) -> toModelMail(m)).collect(Collectors.toList());
+      }
+      try {
+        Thread.sleep(1000);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+  }
+  throw new Error("No mail :(");
+  }
+
+  public static MailMessage toModelMail(WiserMessage m) {
+    try {
+     MimeMessage mm = m.getMimeMessage();
+    }
+  }
+}
--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
@@ -1,23 +1,21 @@
 package ru.stqa.pft.mantis.appmanager;
 
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
 
 /**
  * Created by user on 16.03.2017.
  */
-public class RegistrationHelper {
-  private final ApplicationManager app;
-  private WebDriver wd;
+public class RegistrationHelper extends HelperBase {
+
 
   public RegistrationHelper(ApplicationManager app) {
-    this.app = app;
-    wd = app.getDriver();
-
+    super(app);
   }
 
   public void start(String username, String email) {
     wd.get(app.getProperty("web.baseUrl") + "/signup_page.php");
-
-
+    type(By.name("username"), username);
+    type(By.name("email"), email);
+    click(By.cssSelector("input[value='Зарегистрироваться']"));
   }
 }
